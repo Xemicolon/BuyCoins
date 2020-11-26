dea4a54d89ced2536c5ee115d77a9b84606f964c
Xemicolon <1vibenation@gmail.com>
2020-11-26 23:57:08 +0800

    Minor changes

--- controllers/index.js
+++ controllers/index.js
@@ -33,21 +33,18 @@ exports.getFile = (filePath, res, mimeType) => {
 };
 
 exports.render = (req, res) => {
-  let fileName = path.join(req.url) || "index.html";
+  let fileName =
+    path.join(req.url === "/" ? "/index.html" : req.url) || "index.html";
   let ext = path.extname(fileName);
-  let localFolder = path.dirname(__dirname) + `\\`;
+  let localFolder = path.dirname(__dirname + `\\`);
 
   if (!extensions[ext]) {
-    console.log(fileName);
     res.writeHead(404, {
       "Content-Type": "text/html",
     });
     res.end("Page not found!");
     return;
   }
+  console.log(localFolder + fileName);
   this.getFile(localFolder + fileName, res, extensions[ext]);
-};
-
-exports.hello = () => {
-  console.log("hi");
 };
